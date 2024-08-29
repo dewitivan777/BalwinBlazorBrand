1052735a0538ce29be41c3ecce56d93c31f740cc
dewitivan777 <deWit.Ivan777@gmail.com>
2024-08-29 11:37:47 +0200

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BalwinBlazorBrand
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BalwinBlazorBrand
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
